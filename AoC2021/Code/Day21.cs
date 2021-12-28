141fbfaaf8422a1dd1602423f460d3e31471c3bd
Helen Toomik <helen.toomik@tretton37.com>
2021-12-28 13:15:52 +0100

    Day 21 part 2

--- AoC2021/Code/Day21.cs
+++ AoC2021/Code/Day21.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace AoC2021.Code
@@ -11,15 +12,6 @@ namespace AoC2021.Code
             var p2 = input[1][^1] - 48;
 
             return Simulate(target, p1, p2);
-        }
-
-        public int Solve2(List<string> input)
-        {
-            const int target = 1000;
-            var p1 = input[0][^1] - 48;
-            var p2 = input[1][^1] - 48;
-
-            return 0;
         }
 
         private static int Simulate(int target, int p1, int p2)
@@ -86,6 +78,91 @@ namespace AoC2021.Code
                     return dTotal * s1;
                 }
             }
+        }
+
+        public (long, long) Solve2(List<string> input, int target)
+        {
+            var p1 = input[0][^1] - 48;
+            var p2 = input[1][^1] - 48;
+
+            _cache = new Dictionary<string, Tuple<long, long>>();
+
+            var (wins1, wins2) = TakeTurn(0, 0, p1, p2, true, target);
+
+            return (wins1, wins2);
+        }
+
+        private Dictionary<string, Tuple<long, long>> _cache;
+
+        private Tuple<long, long> TakeTurn(int score1, int score2, int pos1, int pos2, bool p1Turn, int target)
+        {
+            var key = $"{score1}-{score2}-{pos1}-{pos2}-{p1Turn}-{target}";
+            if (_cache.ContainsKey(key))
+            {
+                return _cache[key];
+            }
+
+            if (score1 >= target)
+            {
+                var result = new Tuple<long, long>(1, 0);
+                _cache[key] = result;
+                return result;
+            }
+
+            if (score2 >= target)
+            {
+                var result = new Tuple<long, long>(0, 1);
+                _cache[key] = result;
+                return result;
+            }
+
+            var totalWins1 = 0L;
+            var totalWins2 = 0L;
+
+            for (var r1 = 1; r1 <= 3; r1++)
+            {
+                for (var r2 = 1; r2 <= 3; r2++)
+                {
+                    for (var r3 = 1; r3 <= 3; r3++)
+                    {
+                        var move = r1 + r2 + r3;
+
+                        long wins1;
+                        long wins2;
+                        if (p1Turn)
+                        {
+                            var pos = pos1 + move;
+                            if (pos > 10)
+                            {
+                                pos -= 10;
+                            }
+
+                            var score = score1 + pos;
+
+                            (wins1, wins2) = TakeTurn(score, score2, pos, pos2, !p1Turn, target);
+                        }
+                        else
+                        {
+                            var pos = pos2 + move;
+                            if (pos > 10)
+                            {
+                                pos -= 10;
+                            }
+
+                            var score = score2 + pos;
+
+                            (wins1, wins2) = TakeTurn(score1, score, pos1, pos, !p1Turn, target);
+                        }
+
+                        totalWins1 += wins1;
+                        totalWins2 += wins2;
+                    }
+                }
+            }
+
+            var result2 = new Tuple<long, long>(totalWins1, totalWins2);
+            _cache[key] = result2;
+            return result2;
         }
     }
 }
--- AoC2021/Tests/Test21.cs
+++ AoC2021/Tests/Test21.cs
@@ -31,17 +31,40 @@ Player 2 starting position: 8";
         public void Example2()
         {
             const string data = @"
-";
+Player 1 starting position: 4
+Player 2 starting position: 8";
             var input = DataHelper.SplitLines(data);
-            var result = new Day21().Solve2(input);
-            result.ShouldBe(0);
+            var (r1, r2) = new Day21().Solve2(input, 21);
+            r1.ShouldBe(444356092776315);
+            r2.ShouldBe(341960390180808);
+        }
+
+        [TestCase(1, 27, 0)]
+        [TestCase(2, 183, 156)]
+        [TestCase(3, 990, 207)]
+        [TestCase(4, 2930, 971)]
+        [TestCase(5, 7907, 2728)]
+        [TestCase(6, 30498, 7203)]
+        [TestCase(7, 127019, 152976)]
+        [TestCase(8, 655661, 1048978)]
+        [TestCase(9, 4008007, 4049420)]
+        [TestCase(10, 18973591, 12657100)]
+        public void Example2A(int target, long e1, long e2)
+        {
+            const string data = @"
+Player 1 starting position: 4
+Player 2 starting position: 8";
+            var input = DataHelper.SplitLines(data);
+            var (r1, r2) = new Day21().Solve2(input, target);
+            r1.ShouldBe(e1);
+            r2.ShouldBe(e2);
         }
 
         [Test]
         public void Part2()
         {
-            var input = DataHelper.ReadLines(0);
-            var result = new Day21().Solve2(input);
+            var input = DataHelper.ReadLines(21);
+            var result = new Day21().Solve2(input, 21);
             Console.WriteLine(result);
         }
     }
